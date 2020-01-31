c99fe9ff308df2ae4a3d2b6a21218a6a57fbc622
Rafi'ul Huda <Rafiulhuda27@gmail.com>
2020-01-31 17:35:09 +0700

    melakukantabmodedangeneretetitledangeneretedata

--- app/src/main/java/com/rafi/training/menuapp/MainActivity.java
+++ app/src/main/java/com/rafi/training/menuapp/MainActivity.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager vpku;
     private ViewPagerAnime viewPagerAnime;
     private TabLayout tabfake;
+    private ArrayList<String> titleme, datame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         vpku=findViewById(R.id.vp_fake);
-        viewPagerAnime=new ViewPagerAnime((getSupportFragmentManager()));
+        titleme=new ArrayList<>();
+        datame=new ArrayList<>();
+        genereteData();
+        genereteTitle();
+        viewPagerAnime=ViewPagerAnime.newInstance(getSupportFragmentManager(),titleme,datame);
         tabfake=findViewById(R.id.tab_fake);
         vpku.setAdapter(viewPagerAnime);
         tabfake.setupWithViewPager(vpku);
+    }
+
+    private void genereteData(){
+        datame.add("Data 1");
+        datame.add("Data 2");
+        datame.add("Data 3");
+        datame.add("Data 4");
+        datame.add("Data 5");
+    }
+
+    private void genereteTitle(){
+        titleme.add("Home");
+        titleme.add("Favorite");
+        titleme.add("Genre");
+        titleme.add("episode");
+        titleme.add("anime");
     }
 }
--- app/src/main/java/com/rafi/training/menuapp/ViewPagerAnime.java
+++ app/src/main/java/com/rafi/training/menuapp/ViewPagerAnime.java
@@ -1,5 +1,9 @@
 package com.rafi.training.menuapp;
 
+import android.provider.CalendarContract;
+
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -7,8 +11,20 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
 public class ViewPagerAnime extends FragmentStatePagerAdapter {
+    private static ArrayList<String> myTitleku;
+    private static ArrayList<String> myDataku;
+
+    private static int numOfItemku=0;
+
+    public  static ViewPagerAnime newInstance(FragmentManager fragmentManager, ArrayList title,ArrayList data){
+        ViewPagerAnime viewPagerAnime=new ViewPagerAnime(fragmentManager);
+        myTitleku= title;
+        myDataku=data;
+        numOfItemku=data.size();
+        return viewPagerAnime;
+    }
     public ViewPagerAnime(FragmentManager fragmentManager){
-        super(fragmentManager,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        super(fragmentManager);
     }
     @NonNull
     @Override
@@ -33,27 +49,12 @@ public class ViewPagerAnime extends FragmentStatePagerAdapter {
 
     @Override
     public int getCount() {
-        return 3;
+        return numOfItemku;
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        String title = null;
-        switch (position){
-            case 0:
-                title="Anime";
-                break;
-            case 1:
-                title="Episode";
-                break;
-            case 2:
-                title= "Sinopsis";
-                break;
-             default:
-                 title=null;
-                 break;
-        }
-        return title;
+        return myTitleku.get(position);
     }
 }
